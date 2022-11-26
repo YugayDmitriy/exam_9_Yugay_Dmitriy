8ce8937113f4c90f8dc1fe5f651c414ef43f241d
Dmitriy <Dmitrijugaj614@gmail.com>
2022-11-26 18:18:48 +0600

    Написание частичного кода для добавления в избранное

--- core/Galapp/views.py
+++ core/Galapp/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, get_object_or_404
 import json
 from django.contrib import messages
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from Galapp import forms, models
+
 
 
 def context_data():
@@ -19,111 +18,10 @@ def context_data():
     return context
 
 
-def userregister(request):
-    context = context_data()
-    context['topbar'] = False
-    context['footer'] = False
-    context['page_title'] = "User Registration"
-    if request.user.is_authenticated:
-        return redirect("home-page")
-    return render(request, 'register.html', context)
-
-
 @login_required
 def upload_modal(request):
     context = context_data()
     return render(request, 'upload.html', context)
-
-
-def save_register(request):
-    resp = {'status': 'failed', 'msg': ''}
-    if not request.method == 'POST':
-        resp['msg'] = "No data has been sent on this request"
-    else:
-        form = forms.SaveUser(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Аккаунт успешно создан")
-            resp['status'] = 'success'
-        else:
-            for field in form:
-                for error in field.errors:
-                    if resp['msg'] != '':
-                        resp['msg'] += str('<br />')
-                    resp['msg'] += str(f"[{field.name}] {error}.")
-
-    return HttpResponse(json.dumps(resp), content_type="application/json")
-
-
-@login_required
-def update_profile(request):
-    context = context_data()
-    context['page_title'] = 'Update Profile'
-    user = User.objects.get(id=request.user.id)
-    if not request.method == 'POST':
-        form = forms.UpdateProfile(instance=user)
-        context['form'] = form
-        print(form)
-    else:
-        form = forms.UpdateProfile(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Профиль отредактирован")
-            return redirect("profile-page")
-        else:
-            context['form'] = form
-
-    return render(request, 'manage_profile.html', context)
-
-
-@login_required
-def update_password(request):
-    context = context_data()
-    context['page_title'] = "Update Password"
-    if request.method == 'POST':
-        form = forms.UpdatePasswords(user=request.user, data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Your Account Password has been updated successfully")
-            update_session_auth_hash(request, form.user)
-            return redirect("profile-page")
-        else:
-            context['form'] = form
-    else:
-        form = forms.UpdatePasswords(request.POST)
-        context['form'] = form
-    return render(request, 'update_password.html', context)
-
-
-# Create your views here.
-def login_page(request):
-    context = context_data()
-    context['topbar'] = False
-    context['footer'] = False
-    context['page_name'] = 'login'
-    context['page_title'] = 'Login'
-    return render(request, 'login.html', context)
-
-
-def login_user(request):
-    logout(request)
-    resp = {"status": 'failed', 'msg': ''}
-    username = ''
-    password = ''
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                resp['status'] = 'success'
-            else:
-                resp['msg'] = "Incorrect username or password"
-        else:
-            resp['msg'] = "Incorrect username or password"
-    return HttpResponse(json.dumps(resp), content_type='application/json')
 
 
 @login_required
@@ -136,18 +34,6 @@ def home(request):
     print(context)
     return render(request, 'home.html', context)
 
-
-def logout_user(request):
-    logout(request)
-    return redirect('login-page')
-
-
-@login_required
-def profile(request):
-    context = context_data()
-    context['page'] = 'profile'
-    context['page_title'] = "Profile"
-    return render(request, 'profile.html', context)
 
 
 @login_required
@@ -176,6 +62,15 @@ def view_gallery(request):
     context['page_title'] = "Gallery"
     context['photos'] = models.Gallery.objects.filter(user=request.user, delete_flag=0).all().order_by('-date_created')
     return render(request, 'gallery.html', context)
+
+
+def favorite_photo(request, id):
+    photo = get_object_or_404(models.Gallery, id=id)
+    if photo.favorite.filter(id=request.user.id).exists():
+        photo.favorite.remove(request.user)
+    else:
+        photo.favorite.add(request.user)
+    return HttpResponseRedirect(photo.get_absolute_url())
 
 
 @login_required
